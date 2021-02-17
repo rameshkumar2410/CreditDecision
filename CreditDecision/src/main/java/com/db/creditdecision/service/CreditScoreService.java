de3957252546ffe64cb65ce1d4ddbad3bcb2c5cc
rameshkumar2410 <44162583+rameshkumar2410@users.noreply.github.com>
2021-02-17 19:14:55 +0800

    Modified to add external service

--- CreditDecision/src/main/java/com/db/creditdecision/service/CreditScoreService.java
+++ CreditDecision/src/main/java/com/db/creditdecision/service/CreditScoreService.java
@@ -3,8 +3,13 @@ package com.db.creditdecision.service;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.db.creditdecision.bo.ApplicantDetails;
 import com.db.creditdecision.bo.CreditScore;
 
 /**
@@ -15,6 +20,14 @@ import com.db.creditdecision.bo.CreditScore;
  */
 @Service
 public class CreditScoreService {
+	
+	private static final Logger LOGGER = Logger.getLogger(CreditScoreService.class);
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${app.db.api.getCreditScoreURL}")
+	private String getCreditScoreURL;
 
 	/**
 	 * Mock Data for the Credit Score
@@ -44,6 +57,24 @@ public class CreditScoreService {
 	public List<Integer> getLoanSanctionedSSNNumber() {
 		List<Integer> loanSanctionedSSNList = Arrays.asList(11235, 11230, 11223);
 		return loanSanctionedSSNList;
+	}
+	
+	/**
+	 * 
+	 * @param applicantDetails
+	 * @return
+	 * @throws Exception
+	 */
+	public CreditScore getCreditScore(ApplicantDetails applicantDetails) throws Exception {
+		CreditScore creditscore = null;
+		try {
+			String uri = getCreditScoreURL + applicantDetails.getSsnNumber();
+			creditscore = restTemplate.getForObject(uri, CreditScore.class);
+		} catch (Exception e) {
+			LOGGER.error("Exception Occured inside getCreditScore : " + e.getMessage());
+			throw new Exception(e.getMessage());
+		}
+		return creditscore;
 	}
 
 }
